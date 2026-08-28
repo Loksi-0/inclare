5ba422895f54bf55fec7a8bb5cc4430fd48c2b20
Egor Solovev <egloksi23@gmail.com>
2026-08-28 23:09:31 +0300

    fix(ui): add scroll on moderator tabs to fit in small mobile screens [deploy]

--- apps/frontend/src/widgets/moderator/Me/Me.tsx
+++ apps/frontend/src/widgets/moderator/Me/Me.tsx
@@ -1,12 +1,8 @@
 'use client'
 
 import Avatar from '@/components/Avatar'
-import OptionsButton from '@/components/OptionsButton'
 import cx from 'clsx'
-import { useTRPC } from '@/api/tanstack'
-import { useMutation } from '@tanstack/react-query'
-import { useNavigate } from '@/shared/hooks/useNavigate'
-import { DEFAULTS, PAGES } from '@/constants'
+import { PAGES } from '@/constants'
 import Button from '@/components/Button'
 import styles from './Me.module.scss'
 
@@ -17,24 +13,6 @@ type MeProps = {
 }
 
 const Me = ({ avatar, name, role }: MeProps) => {
-  const { replace } = useNavigate()
-
-  const trpc = useTRPC()
-  const { mutate: logoutCurrent, isPending: isCurrentPending } = useMutation(
-    trpc.auth.logoutCurrentDevice.mutationOptions({
-      onSuccess: () => {
-        replace(DEFAULTS.START_PAGE)
-      }
-    })
-  )
-  const { mutate: logoutAll, isPending: isAllPending } = useMutation(
-    trpc.auth.logoutAll.mutationOptions({
-      onSuccess: () => {
-        replace(DEFAULTS.START_PAGE)
-      }
-    })
-  )
-
   return (
     <section className={styles.me}>
       <div className={styles.me__body}>
